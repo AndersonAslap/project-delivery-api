c8b409e667df9516cb4a0156078c3c515ebe1fc7
Anderson Adolfo <anderson.adolfo1998@gmail.com>
2022-03-28 22:02:25 -0300

    feat: create test unitary

--- src/modules/client/test/createClient/CreateClientUseCase.spec.ts
+++ src/modules/client/test/createClient/CreateClientUseCase.spec.ts
@@ -16,4 +16,10 @@ describe("create-client-use-case", () => {
         const client = await createClientUseCase.execute(clientDTO);
         expect(client.username).toBe("Aslap");
     });
+
+    it("should not be able create new client", async () => {
+        expect(async () => {
+            await createClientUseCase.execute(clientDTO);
+        }).rejects.toBeInstanceOf(Error);
+    });
 });
