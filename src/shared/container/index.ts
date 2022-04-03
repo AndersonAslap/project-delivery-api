ab929cbd8290367350c938cde924e3778ac0c96a
Anderson Adolfo <anderson.adolfo1998@gmail.com>
2022-04-03 03:08:03 -0300

    feat: create register singleton of the deliveryman repository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,8 +2,15 @@ import { container } from "tsyringe";
 
 import { ClientsRepository } from "../../modules/client/infra/prismaorm/repositories/ClientsRepository";
 import { IClientsRepository } from "../../modules/client/repositories/IClientsRepository";
+import { DeliverymansRepository } from "../../modules/deliveryman/infra/prismaorm/repositories/DeliverymansRepository";
+import { IDeliverymansRepository } from "../../modules/deliveryman/repositories/IDeliverymansRepository";
 
 container.registerSingleton<IClientsRepository>(
   "ClientsRepository",
   ClientsRepository
+);
+
+container.registerSingleton<IDeliverymansRepository>(
+  "DeliverymansRepository",
+  DeliverymansRepository
 );
