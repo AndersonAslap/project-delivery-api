2dde8493cd403bf924bcc34e0a4b77e976d26fcc
Anderson Adolfo <anderson.adolfo1998@gmail.com>
2022-04-03 02:44:32 -0300

    feat: implemetation of the use case of the create deliveryman

--- src/modules/deliveryman/test/createDeliveriman/CreateDeliverymanUseCase.spec.ts
+++ src/modules/deliveryman/test/createDeliveriman/CreateDeliverymanUseCase.spec.ts
@@ -1,2 +1,6 @@
+import { DeliverymansRepositoryInMemory } from "../../repositories/in-memory/DeliverymansRepositoryInMemory";
+
 let deliverymansRepositoryInMemory: DeliverymansRepositoryInMemory;
+let createDeliverymanUseCase: CreateDeliveryUseCase;
+
 describe("Create Deliveryman", () => { });
---
+++ src/modules/deliveryman/useCases/CreateDeliverymanUseCase.ts
@@ -0,0 +1,36 @@
+import { hash } from "bcrypt";
+import { inject, injectable } from "tsyringe";
+import { AppError } from "../../../shared/error/AppError";
+import { Deliveryman } from "../infra/prismaorm/entities/Deliveryman";
+import { IDeliverymansRepository } from "../repositories/IDeliverymansRepository";
+
+interface IRequest {
+    username: string;
+    password: string;
+}
+
+@injectable()
+class CreateDeliverymanUseCase {
+
+    constructor(
+        @inject("DeliverymansRepository")
+        private deliverymansRepository: IDeliverymansRepository
+    ) { }
+
+    async execute({ username, password }: IRequest): Promise<Deliveryman> {
+
+        const deliverymanAlreadyExists = this.deliverymansRepository.findByUsername(username);
+
+        if (deliverymanAlreadyExists) {
+            throw new AppError("Deliveryman already exists!");
+        }
+
+        const hashPassword = await hash(password, 10);
+
+        const deliveryman = this.deliverymansRepository.create({ username, password: hashPassword });
+
+        return deliveryman;
+    }
+}
+
+export { CreateDeliverymanUseCase };
