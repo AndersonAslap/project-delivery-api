d87d8fc537fe7d4a9fda09ef1c73370ad21beec9
Anderson Adolfo <anderson.adolfo1998@gmail.com>
2022-04-05 20:43:56 -0300

    feat: create delivery routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { clientsRoutes } from "./client.routes";
+import { deliveryRoutes } from "./delivery.routes";
 import { deliverymanRoutes } from "./deliveryman.routes";
 
 const routes = Router();
 
 routes.use("/client", clientsRoutes);
 routes.use("/deliveryman", deliverymanRoutes);
+routes.use("/deliveries", deliveryRoutes);
 
 export { routes };
